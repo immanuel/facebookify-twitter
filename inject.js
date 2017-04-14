2f7886921ee11c9bc9a3cf8a452da503edd93122
Immanuel Thomas <immanuel278@gmail.com>
2017-04-14 15:37:57 -0700

    Fixed bug for new frames
    - Content script requests current state of toggle from background script
    - Fixes the issue for frames injected after facebookification

--- inject.js
+++ inject.js
@@ -3,16 +3,16 @@ style.rel = 'stylesheet';
 style.type = 'text/css';
 style.href = chrome.extension.getURL('facebookify.css');
 
-var inserted = false;
+// After coming online first time, check the background page for current state
+chrome.runtime.sendMessage({msg: "getStatus"}, function(response) {
+    if(response.currState)
+        (document.head||document.documentElement).appendChild(style);
+});
 
 // Toggle style tag in the DOM when the background script pings
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (!inserted) {
+    if (request.newState)
         (document.head||document.documentElement).appendChild(style);
-        inserted = true;
-    }
-    else {
+    else
         (document.head||document.documentElement).removeChild(style);
-        inserted = false;
-    }
 });
--- toggle.js
+++ toggle.js
@@ -1,6 +1,15 @@
-// Respond to badge click event
+var inserted = false;
+
+// Inform content script of badge click event
 chrome.browserAction.onClicked.addListener(function(tab) { 
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {toggle: true});
+        chrome.tabs.sendMessage(tabs[0].id, {newState: !inserted});
+        inserted = !inserted
     });
+});
+
+// Respond to query from content script for curr state
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.msg == "getStatus")
+        sendResponse({currState: inserted});
 });
