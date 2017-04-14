37fb7f1e87e1f650e8b8ef67e9fcc5c64110519e
Immanuel Thomas <immanuel278@gmail.com>
2017-04-14 01:29:49 -0700

    Toggle facebookification by clicking on icon in toolbar

--- inject.js
+++ inject.js
@@ -1,7 +1,18 @@
-// Insert the css at the end to override existing styles
-// TODO: Causes flicker
 var style = document.createElement('link');
 style.rel = 'stylesheet';
 style.type = 'text/css';
 style.href = chrome.extension.getURL('facebookify.css');
-(document.head||document.documentElement).appendChild(style);
+
+var inserted = false;
+
+// Toggle style tag in the DOM when the background script pings
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (!inserted) {
+        (document.head||document.documentElement).appendChild(style);
+        inserted = true;
+    }
+    else {
+        (document.head||document.documentElement).removeChild(style);
+        inserted = false;
+    }
+});
---
+++ toggle.js
@@ -0,0 +1,6 @@
+// Respond to badge click event
+chrome.browserAction.onClicked.addListener(function(tab) { 
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {toggle: true});
+    });
+});
